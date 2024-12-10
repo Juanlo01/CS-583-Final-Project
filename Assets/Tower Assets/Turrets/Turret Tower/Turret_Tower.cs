6d6604bc4b662057de1e2ccd5fb232da57a48c7d
Kevin Ruvalcaba <127262121+Kruvalcaba85@users.noreply.github.com>
2024-12-10 02:06:53 -0800

    Implemented shooting mechanic to towers, hit particles needed next

--- Assets/Tower Assets/Turrets/Turret Tower/Turret_Tower.cs
+++ Assets/Tower Assets/Turrets/Turret Tower/Turret_Tower.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class Turret_Tower : MonoBehaviour
 {
     public Transform target;
+
+    [Header("Attributes")]
+
     public float range = 3f;
+    public float fireRate = 1f;
+    private float fireCountDown = 0f;
+
+    [Header("Unity Setup Fields")]
 
     public string enemyTag = "Enemy";
-
     public Transform partToRotate;
     public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     // Start is called before the first frame update
     void Start()
@@ -57,6 +66,26 @@ public class Turret_Tower : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountDown <= 0f)
+        {
+            Shoot();
+            fireCountDown = 1f / fireRate;
+
+        }
+
+        fireCountDown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet bullet = bulletGO.GetComponent<bullet>();
+
+        if (bullet != null)
+        {
+            bullet.Seek(target);
+        }
     }
 
     //function needed to visualize the range of our turret
