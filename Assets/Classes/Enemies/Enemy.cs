ccdb096097c6f54b8ccf68af0793c0f3df247ba4
Juanlo01 <jcota3264@gmail.com>
2024-12-12 14:08:02 -0800

    All towers purchasable through buttons
    You can now purchase all the towers in game, however no economy is in place, just yet.

--- Assets/Classes/Enemies/Enemy.cs
+++ Assets/Classes/Enemies/Enemy.cs
@@ -15,5 +15,11 @@ public class Enemy : MonoBehaviour{
         transform.position = GameLoopManager.NodePositions[0];
         NodeIndex = 0;
     }
+
+    // Called from the bullet script
+    public void TakeDamage(int damageAmount){
+        Health -= damageAmount;
+         Debug.Log("Enemy Took 1 damage");
+    }
     
 }
